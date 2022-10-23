191cac3a93ed04e047d961a7f89af369d49252f7
vugsk <ganchukovnikita@bk.ru>
2022-10-23 18:12:16 +0700

    upt

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# OS_SIBERIA_CORE_SFRC_ALPHA
- ОС Сибирь ядро Совет Сободных Республик Ядро
+# OS SIBERIA
+ ОС Сибирь
--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 nasm -f elf32 OS/boot/boot.s -o build/kasm.o
 
-gcc -m32 -c OS/kernel/kernel.c -o build/kc.o
-gcc -m32 -c OS/kernel/sys.c -o build/cmd/sys.o
-gcc -m32 -c OS/kernel/common.c -o build/cmd/common.o
-gcc -m32 -c OS/kernel/monitor.c -o build/cmd/monitor.o
+gcc -m32 -c OS/kernel_SFRC/kernel.c -o build/kc.o
+gcc -m32 -c OS/kernel_SFRC/sys.c -o build/cmd/sys.o
+gcc -m32 -c OS/kernel_SFRC/common.c -o build/cmd/common.o
+gcc -m32 -c OS/kernel_SFRC/monitor.c -o build/cmd/monitor.o
 
 ld -m elf_i386 -T build/link.ld -o kernel build/kasm.o build/kc.o build/cmd/sys.o build/cmd/common.o build/cmd/monitor.o
 
