25900cb5f1ed616ca7ea59720a1808b65f7d9e11
vugsk <ganchukovnikita@bk.ru>
2022-10-23 18:14:05 +0700

    upt

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-nasm -f elf32 OS/boot/boot.s -o build/kasm.o
+nasm -f elf32 boot/boot.s -o build/kasm.o
 
-gcc -m32 -c OS/kernel_SFRC/kernel.c -o build/kc.o
-gcc -m32 -c OS/kernel_SFRC/sys.c -o build/cmd/sys.o
-gcc -m32 -c OS/kernel_SFRC/common.c -o build/cmd/common.o
-gcc -m32 -c OS/kernel_SFRC/monitor.c -o build/cmd/monitor.o
+gcc -m32 -c kernel_SFRC/kernel.c -o build/kc.o
+gcc -m32 -c kernel_SFRC/sys.c -o build/cmd/sys.o
+gcc -m32 -c kernel_SFRC/common.c -o build/cmd/common.o
+gcc -m32 -c kernel_SFRC/monitor.c -o build/cmd/monitor.o
 
 ld -m elf_i386 -T build/link.ld -o kernel build/kasm.o build/kc.o build/cmd/sys.o build/cmd/common.o build/cmd/monitor.o
 
